61559a6ca9db1e70be2fa88c662bcf87e52301e1
Zlobrynya <zlobrynya@gmail.com>
2020-01-13 09:34:41 +0300

    Create project

---
+++ Portfolio/ViewController/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio
+//
+//  Created by Nikitin Nikita on 13/01/2020.
+//  Copyright © 2020 Zlobrynya. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
